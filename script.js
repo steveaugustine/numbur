2e77c2773e4cdff3c4dc5ca350ab7f1eab28c835
enter-opy <vivekvijayan6282@gmail.com>
2022-02-10 21:11:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 
 var input = document.getElementById("input");
-var count = 0; // keep track of chance
+var count = 0; // keep track of chances
 
-const numbur = Math.floor(Math.random() * 90) + 10; // random number
+let numbur = Math.floor(Math.random() * 90) + 10; // random number
 console.log("random number: " + numbur)
 
+while (isaPrime(numbur)) {
+    numbur =  Math.floor(Math.random() * 90) + 10;
+    console.log("random number: " + numbur)
+} 
 
 input.addEventListener("keydown", function (event) {
     // function on enter key press and input not empty
-    if (event.code === "Enter" && !isNaN(input.value)) {
+    if ((event.code === "Enter" || event.keyCode == 13) && !isNaN(input.value)) {
         count += 1;
     
         if (numbur == parseInt(input.value)) {
@@ -40,3 +44,13 @@ input.addEventListener("keydown", function (event) {
         }
     }
 });
+
+function isaPrime(number) {
+    for (let i = 2; i <= number / 2; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
