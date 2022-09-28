fa1e095d5884df5d113421bc8638225566990847
Steve Augustine <steveaugustine9@gmail.com>
2022-09-28 20:15:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -116,15 +116,7 @@ input.addEventListener("keydown", function (event) {
     }
 });
 
-function isaPrime(number) {
-    for (let i = 2; i <= number / 2; i++) {
-        if (number % i == 0) {
-            return false;
-        }
-    }
 
-    return true;
-}
 
     
     });
